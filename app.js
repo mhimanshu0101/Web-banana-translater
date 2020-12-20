7b2284c3913abd155c81d17a433973f8cd1c32ae
Martial Himanshu <mhimanshu0101@gmail.com>
2020-12-21 03:19:45 +0530

    style- querySelector multiple lookup check

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+// querySelector gives the first one with lookup value
+// querySelectorAll returns all matching lookup value
 
 console.log(txtInput)
 // Click handler
@@ -9,3 +11,9 @@ function clickHandler(){
 
 // arguments: event name, function name
 btnTranslate.addEventListener("click", clickHandler);
+
+// querySelector() works on the principles of CSS selectors. It selects and returns the first element which matches the query.
+var txtQuery = document.querySelector("textarea");
+var txtQuery = document.querySelector(".btn-primary");
+var txtQuery = document.querySelector("#input-btn");
+var txtQuery = document.querySelector(input[name="translator"]);

9976952e57c98f6d71925655558c3270d3b8ae53
Martial Himanshu <mhimanshu0101@gmail.com>
2020-12-21 03:33:13 +0530

    feat- Input/output from textarea to div

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input");
 // querySelector gives the first one with lookup value
 // querySelectorAll returns all matching lookup value
 
-console.log(txtInput)
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
 // Click handler
 function clickHandler(){
-    console.log(txtInput.value);
+    outputDiv.innerText = "banana language";
 }
 
 // arguments: event name, function name
 btnTranslate.addEventListener("click", clickHandler);
-
-// querySelector() works on the principles of CSS selectors. It selects and returns the first element which matches the query.
-var txtQuery = document.querySelector("textarea");
-var txtQuery = document.querySelector(".btn-primary");
-var txtQuery = document.querySelector("#input-btn");
-var txtQuery = document.querySelector(input[name="translator"]);

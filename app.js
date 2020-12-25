8b081f92c24233e5b0dc82ccb54b2304fcb54e36
Martial Himanshu <mhimanshu0101@gmail.com>
2020-12-25 16:38:07 +0530

    feat - Error handling, refr- style.css

--- app.js
+++ app.js
@@ -4,10 +4,31 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
+// console.log(outputDiv)
+
+var url = "https://api.funtranslations.com/translate/minion.json?text="
+
+function errorHandled(error){
+    alert("Something has broked, we will be back soon.")
+}
+// callback function
+function translate(text){
+    fetch(url+text).then(response => response.json()).then(
+        json => {
+            outputDiv.innerText = json.contents.translated;
+        })
+        .catch(errorHandled)
+}
 
 // Click handler
 function clickHandler(){
-    outputDiv.innerText = "banana language";
+    if (txtInput.value === ""){
+        alert("Write Something.")
+        return
+    }
+    else{
+    translate(txtInput.value)
+    }
 }
 
 // arguments: event name, function name
